9b7a6f1f815556e29d241e9cfcfa6c1c3d59a5de
yootao <xmzyt1996@gmail.com>
2016-08-05 21:37:45 +0800

    SG函数

--- SG函数.cpp
+++ SG函数.cpp
@@ -1,18 +1,20 @@
-int s[N], sg[N];
-bool hs[N];
+int sg[105];
 
-void getSG(int* s, int len, int N) {
-    memset(sg, 0, sizeof(sg));
-    for (int i = 0; i < N; ++i) {
-        memset(hs, 0, sizeof(hs));
-        for (int j = 0; j < len; ++j) {
-            if (i - s[j] < 0) continue;
-            hs[sg[i - s[j]]] = 1;
-        }
-        for (int j = 0; j <= i; ++j) 
-            if (!hs[j]) {
-                sg[i] = j; 
-                break;
-            }
+int getSG(int x) {
+    if (sg[x] >= 0) return sg[x];
+    set<int> s;
+    // 转移到[0, x) 任意一种情况
+    rep(i, 0, x) s.insert(getSG(i));
+    // 分成三堆的情况
+    rep(i, 1, x) rep(j, 1, x) rep(k, 1, x) {
+        if (i + j + k == x) s.insert(getSG(i) ^ getSG(j) ^ getSG(k));
     }
+    int i; for (i = 0; s.count(i); ++i);
+    sg[x] = i;
+    return sg[x];
+}
+
+void solve() {
+    clr(sg, -1);
+    rep(i, 1, 100) printf("sg[%d] = %d\n", i, getSG(i));
 }
