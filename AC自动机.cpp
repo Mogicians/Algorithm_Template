8300b9b6573025c9a2c422671884b0fc25849987
ZhengYangtao <xmzyt1996@gmail.com>
2016-09-27 21:52:21 +0800

    Update AC自动机.cpp

--- AC自动机.cpp
+++ AC自动机.cpp
@@ -1,61 +1,67 @@
-const int SIZE = 26;
-
 struct AhoCorasick {
-	int next[MAX_N][SIZE];
-	int sz, val[MAX_N], fail[MAX_N], last[MAX_N];
-    
-	void init() { sz = 1; clr(next[0], 0); }
-	int idx(char c) { return c - 'a'; }
-	int newnode() { clr(next[sz], 0); val[sz] = 0; return sz++; }
-	
-	void insert(char* s, int v = 1) {
-	    int cur = 0;
-	    while(*s) {
-	    	int id = idx(*s++);
-	    	if (!next[cur][id]) next[cur][id] = newnode();
-			cur = next[cur][id];
-		}
-		val[cur] = v;
-	}
-	
-	void getfail() {
-		queue<int> q;
-		fail[0] = 0;
-		rep(c, 0, SIZE) {
-			int u = next[0][c];
-			if (u) { fail[u] = 0; q.push(u); last[u] = 0; }
-		}
-		while (!q.empty()) {
-			int r = q.front(); q.pop();
-			rep(c, 0, SIZE) {
-				int u = next[r][c];
-				if (!u) {
-					next[r][c] = next[fail[r]][c]; //建成Trie图
-					continue;
-				}
-				q.push(u);
-				int v = fail[r];
-				while (v && !next[v][c]) v = fail[v];
-				fail[u] = next[v][c];
-				last[u] = (val[fail[u]] ? fail[u] : last[fail[u]]);
-			}
-		}
-	}
-	
-	void find(char* T) {
-		int j = 0;
-		while(*T) {
-			int c = idx(*T++);
-			j = next[j][c];
-			if (val[j]) out(j);
-			else if (last[j]) out(last[j]);
-		}
-	}
-	
-	void out(int i) {
-		if (i) {
-			cnt++;
-			out(last[i]);
-		}
-	}   
-} AC;
+
+    int next[N][26], fail[N], end[N], vis[N];
+    int root, sz;
+
+    int newnode() {
+        clr(next[sz], -1);
+        end[sz] = 0;
+        return sz++;
+    }
+
+    void init() {
+        sz = 0;
+        clr(vis, 0);
+        root = newnode();
+    }
+
+    int idx(char c) { return c - 'a'; }
+
+    void insert(char* s) {
+        int now = root;
+        while (*s) {
+            int id = idx(*s++);
+            if (next[now][id] == -1)
+                next[now][id] = newnode();
+            now = next[now][id];
+        }
+        end[now]++;
+    }
+
+    void build() {
+        queue<int> Q;
+        fail[root] = root;
+        rep(i, 0, 26) {
+            if (next[root][i] != -1) {
+                fail[next[root][i]] = root;
+                Q.push(next[root][i]);
+            } else next[root][i] = root;
+        }
+        while(!Q.empty()) {
+            int now = Q.front(); Q.pop();
+            rep(i, 0, 26) {
+                if (next[now][i] != -1) {
+                    fail[next[now][i]] = next[fail[now]][i];
+                    Q.push(next[now][i]);
+                } else next[now][i] = next[fail[now]][i];
+            }
+        }
+    }
+
+    int query(char* s){
+        int now = root;
+        int res = 0;
+        while (*s) {
+            int id = idx(*s++);
+            now = next[now][id];
+            int temp = now;
+            while(temp != root && !vis[temp]) {
+                res += end[temp];
+                end[temp] = 0;
+                vis[temp] = 1;
+                temp = fail[temp];
+            }
+        }
+        return res;
+    }
+} ac;
