3fe93b93f8909cdb22ca0b2fa9430884a7541a3d
ZhengYangtao <xmzyt1996@gmail.com>
2016-09-27 21:52:58 +0800

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,33 +1,42 @@
-const int SIZE = 26;
+
 struct Trie {
-	int next[MAX_N][SIZE];
-	int sz, val[MAX_N];
-    
-    void init() { sz = 1; clr(next[0], 0); }
-	Trie() { init(); }
+
+	int next[N][26];
+	int root, sz, val[N];
+
 	int idx(char c) { return c - 'a'; }
-	
+
+    int newnode() {
+        clr(next[sz], -1);
+        val[sz] = 0;
+        return sz++;
+    }
+
+    void init() {
+        sz = 0;
+        root = newnode();
+    }
+
+    Trie() { init(); }
+
 	void insert(char* s, int v = 1) {
-	    int cur = 0;
+	    int now = root;
 	    while(*s) {
 	    	int id = idx(*s++);
-	    	if (!next[cur][id]) {
-	    		clr(next[sz], 0);
-	    		val[sz] = 0;
-	    		next[cur][id] = sz++;
-			}
-			cur = next[cur][id];
+	    	if (next[now][id] == -1)
+	    		next[now][id] = newnode();
+			now = next[now][id];
 		}
-		val[cur] = v;
+		val[now] = v;
 	}
-	
-	bool search(char* s) {
-		int cur = 0;
+
+	int query(char* s) {
+		int now = root;
 		while(*s) {
 			int id = idx(*s++);
-			if (!next[cur][id]) return false;
-			cur = next[cur][id];
+			if (next[now][id] == -1) return 0;
+			now = next[now][id];
 		}
-		return val[cur];
-	}     
-} tire;
+		return val[now];
+	}
+} trie;
